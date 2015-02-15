e815ef7e5de5d75619daf9f7db3eef007a3f1390
Keirron Stach <stachkeirron@gmail.com>
2015-02-15 22:41:00 +1030

    Character movement

--- Game/Assets/Scripts/CharacterController.cs
+++ Game/Assets/Scripts/CharacterController.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(MovementController))]
 public class CharacterController : MonoBehaviour {
+
+    MovementController moveController;
 
 	// Use this for initialization
 	void Start () {
-	
+        moveController = GetComponent<MovementController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void FixedUpdate()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        Vector2 moveV = new Vector2(x, y);
+        if(moveV != Vector2.zero)
+        {
+            moveController.Move(moveV);
+        }
+
+        Vector2 mp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        moveController.RotateTowards(mp);
+    }
 }
---
+++ Game/Assets/Scripts/MovementController.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class MovementController : MonoBehaviour {
+
+    public float MoveForce;
+    public float MaxRotation;
+    public Rigidbody2D body;
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void MoveForward()
+    {
+        MoveVertical(1);
+    }
+
+    public void MoveBackward()
+    {
+        MoveVertical(-1);
+    }
+
+    public void MoveVertical(float power)
+    {
+        if (power > 1) power = 1;
+        if (power < -1) power = -1;
+        Move(new Vector2(0, power));
+    }
+
+    public void MoveRight()
+    {
+        MoveHorizontal(1);
+    }
+
+    public void MoveLeft()
+    {
+        MoveHorizontal(-1);
+    }
+
+    public void MoveHorizontal(float power)
+    {
+        if (power > 1) power = 1;
+        if (power < -1) power = -1;
+        Move(new Vector2(power, 0));
+    }
+
+    public void Move(Vector2 direction)
+    {
+        direction.Normalize();
+        body.AddForce(direction * MoveForce);
+    }
+
+    
+
+
+
+    public void RotateTowards(Vector2 pos)
+    {
+        
+    }
+
+
+}
