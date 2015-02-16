bd31d7cbd50043b825f7ecaea3ae0b026e6cf219
CaptanSivey <jimbastiras@gmail.com>
2015-02-17 00:11:58 +1030

    Added Health, Damage and Layers

---
+++ Game/Assets/Scripts/Health.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour 
+{
+
+	public float health;
+	public float maxHealth;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	public void takeDamage(float damage)
+	{
+		health -= damage;
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+		if (health > maxHealth) 
+		{
+			health = maxHealth;
+		}
+
+		if (health <= 0)
+		{
+			Destroy(gameObject);
+		}
+	
+	}
+}
--- Game/Assets/Scripts/Projectile.cs
+++ Game/Assets/Scripts/Projectile.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Projectile : MonoBehaviour 
 {
 	public float speed;
+	public float damage;
 	private Vector3 dir;
 	void Start ()
 	{
@@ -17,20 +18,23 @@ public class Projectile : MonoBehaviour
 	}
 	void OnTriggerEnter2D(Collider2D other) 
 	{
-		if (!(other.tag == "Player")) 
+
+		if (other.gameObject.layer == 8) 
 		{
-			if (other.gameObject.tag == "Enemy") 
+			if (other.gameObject.GetComponent<Health>()!= null)
 			{
-				Destroy (other.gameObject);
-				Destroy(gameObject);
-			}
-			if (other.gameObject.tag == "Border")
-			{
-				Destroy(gameObject);
+				other.gameObject.GetComponent<Health>().takeDamage(damage);
 			}
 
+			Destroy(gameObject);
+		}
 
+		if (other.gameObject.layer == 9)
+		{
+			Destroy(gameObject);
 		}
 
 	}
+
 }
+
