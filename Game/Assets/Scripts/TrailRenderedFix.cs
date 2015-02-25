57162f045782502c7858315526e6c86d7d572428
Keirron Stach <stachkeirron@gmail.com>
2015-02-25 19:57:52 +1030

    Trail Renderer Fix
    fixed up trail Renderer

---
+++ Game/Assets/Scripts/TrailRenderedFix.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrailRenderedFix : MonoBehaviour {
+
+    private TrailRenderer trail;
+
+    void Start()
+    {
+        trail = GetComponent<TrailRenderer>();
+    }
+
+	void OnEnable()
+    {
+        Debug.Log("Enabled");
+        StartCoroutine(ResetTrail());
+    }
+
+    private IEnumerator ResetTrail()
+    {
+        float time = trail.time;
+        trail.time = 0;
+        yield return new WaitForEndOfFrame();
+        trail.time = time;
+    }
+}
