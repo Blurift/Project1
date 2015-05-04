31a000dea4368c40f1c606cd1dcb48448655b5ef
Keirron Stach <stachkeirron@gmail.com>
2015-05-04 23:53:39 +0930

    Start of Level Selector

---
+++ Game/Assets/Scripts/GameSave.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class GameSave
+{
+    public Level[] Levels;
+
+    public int CompletedLevels()
+    {
+        int count = 0;
+        for (int i = 0; i < Levels.Length; i++)
+        {
+            if (Levels[i].ConquerCompleted)
+                count++;
+        }
+        return count;
+    }
+
+}
+
+[System.Serializable]
+public class Level
+{
+    public string LevelName = "";
+    public int LevelsToUnlock = 0;
+    public bool ConquerCompleted = false;
+    public int ScoreFromSurvival = 0;
+}
+
