eab78168f06549e6be6cdd99c0d6c24246b07bea
Keirron Stach <stachkeirron@gmail.com>
2015-05-06 09:34:27 +0930

    Level Selector Prefabs

---
+++ Game/Assets/Scripts/Menu/LevelMenuItem.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LevelMenuItem : MonoBehaviour {
+
+    public Image LevelImage;
+    public Text LevelName;
+    public Text LevelScore;
+    public Toggle ConqueredToggle;
+
+    public Button Button;
+
+	public void SetLevel(Sprite image, string name, int score, bool conquered, bool unlocked)
+    {
+        LevelImage.sprite = image;
+        LevelName.text = name;
+        LevelScore.text = "Score: " + score;
+        ConqueredToggle.isOn = conquered;
+        Button.interactable = unlocked;
+    }
+
+    public void OnClick()
+    {
+
+    }
+}
